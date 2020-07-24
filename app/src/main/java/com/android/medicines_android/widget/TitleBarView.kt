58c78686b6bf195a847985f36f45eda98acae5a1
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-24 17:00:09 +0600

    Create TitleBarView.kt

---
+++ app/src/main/java/com/android/medicines_android/widget/TitleBarView.kt
@@ -0,0 +1,13 @@
+package com.android.medicines_android.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class TitleBarView: LinearLayout {
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}

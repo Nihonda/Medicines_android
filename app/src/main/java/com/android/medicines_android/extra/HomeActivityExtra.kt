d230108a655c09f5b8eb86e8a8f4a06b7c4a229d
OkonovUrmat <okonov.okonov@gmail.com>
2020-07-24 16:59:29 +0600

    Create HomeActivityExtra

---
+++ app/src/main/java/com/android/medicines_android/extra/HomeActivityExtra.kt
@@ -0,0 +1,6 @@
+package com.android.medicines_android.extra
+
+import com.android.medicines_android.widget.tabbar.TabBarItemEnum
+import java.io.Serializable
+
+data class HomeActivityExtra(val tabBarItem: TabBarItemEnum?) : Serializable
